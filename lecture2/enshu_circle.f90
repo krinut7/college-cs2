3e36b22fe8bab01c477aa301f2ac4b8e0ea93740
krishna-aobab <krinut7@gmail.com>
2021-06-24 14:29:57 +0900

    dance

--- lecture2/enshu_circle.f90
+++ lecture2/enshu_circle.f90
@@ -1,19 +1,19 @@
-program enshu_1
+program enshu_circle
       ! author: Krishna Soni (B8TB1707)
       ! a program to calculate the area of cirlce of radius 1, x^2 + y^2 = 1
       ! we would calculate the area of a quarter of a circle using intergration
       ! and multiply it by 4 at the end. 
       implicit none
-      integer, parameter :: N=100   ! number of small rectangle to be used
+      integer, parameter :: N=10000   ! number of small rectangle to be used
       real, parameter :: pi = 3.14      
       integer :: i      ! interative variable
       real :: area=0, tharea, f, dx, a=0, b=1        ! [a,b] is the domain of the integration    
-      dx = abs((b-a)/N)
+      dx = 1.0/N
       do i = 1, N
-        f = sqrt(1 - (dx*i+a)**2)
+        f = sqrt(1 - (dx*i)*(dx*i))
         area = area + f*dx
       enddo
       area = area*4     ! area of complete circle
       tharea = pi
       print *, "area = ", area, "theoritical area = ", tharea
-endprogram enshu_1
+endprogram enshu_circle
--- lecture2/enshu_sphere.f90
+++ lecture2/enshu_sphere.f90
@@ -1,27 +1,33 @@
-program enshu_1
+program enshu_sphere
       ! author: Krishna Soni (B8TB1707)
-      ! a program to calculate the area of cirlce of radius 1, x^2 + y^2 = 1
-      ! we would calculate the area of a quarter of a circle using intergration
-      ! and multiply it by 4 at the end. 
+      ! a program to calculate the volume of sphere of radius 1, x^2 + y^2 +z^2 = 1
+      ! the basic idea is to find the volume by dividing the sphere into small cylinders. 
+      ! to find the volume of the cylinder, we would find the area of a circle at a point z and 
+      ! then multiply it with dz. The area for that cylinder will be found using the same algorithm as
+      ! enshu_circle
+
       implicit none
-      integer, parameter :: N=100   ! number of small rectangle to be used
+      integer, parameter :: N=10000   ! number of small rectangle to be used
       real, parameter :: pi = 3.14      
-      integer :: i,j      ! interative variable
-      real :: area, volume=0, thvolume, f, dx, dz, a=0, b, r=1        ! [a,r] is the domain of the integration
-      dz = (r-a)/N
-      do j = N-1, 0, -1
-        b = dz*j    
-        dx = abs((b-a)/N)
+      integer :: i,j      ! iterative variable
+      double precision :: area, volume=0, thvolume, f, dx, dz, a=0, b, r=1  ! here b is the radius of circle at z
+      dz = 1.0/N
+      do j = N, 0, -1
+        b = (1.0/N)*j    
+        dx = b/N
         area = 0
         do i = 1, N
-                f = sqrt(1 - (dx*i+a)**2)
+                f = sqrt(1 - (dx*i)*(dx*i))
                 area = area + f*dx
         enddo
         area = area*4     ! area of complete circle
+        if (j<100) then
+        print *, "area", j, "==", area
+        endif
         volume = volume + area*dz
       enddo
       volume = volume*2
       thvolume = pi*(4.0/3.0)
       print *, "volume = ", volume, "theoritical volume = ", thvolume
       
-endprogram enshu_1
+endprogram enshu_sphere
